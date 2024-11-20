4ec6a8ae724dc6f004d80433f0d31e73f82dce6d
Gilbert Acosta <gisawebdev@gmail.com>
2024-11-20 14:29:46 -0400

    update

---
+++ introCsharpBackend/introCsharpBackend/Program.cs
@@ -0,0 +1,48 @@
+﻿
+
+var sale = new SaleWithTax(15, 1.16m);
+var message = sale.GetInfo();
+
+Console.WriteLine(message);
+
+
+class SaleWithTax : Sale
+{
+
+    public decimal Tax { get; set; }
+    public SaleWithTax(decimal total, decimal tax) : base(total)
+    {
+        Tax = tax;
+    }
+
+    public override string GetInfo()
+    {
+        return "El total es " + Total + " y el impuesto es " + Tax;
+    }
+
+
+    // Sobrecarga
+    public string GetInfo(string message)
+    {
+        return message;
+    }
+
+
+}
+
+
+class Sale
+{
+    public decimal Total { get; set; }
+
+    public Sale(decimal total)
+    {
+        Total = total;
+    }
+
+    public virtual string GetInfo()
+    {
+        return "El total es " + Total;
+    }
+
+}
