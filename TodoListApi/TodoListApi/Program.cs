8597727dee2cdc57cec811b217d08be9f0c17308
Gilbert Acosta <gisawebdev@gmail.com>
2025-01-04 10:37:34 -0400

    update

--- TodoListApi/TodoListApi/Program.cs
+++ TodoListApi/TodoListApi/Program.cs
@@ -1,6 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using TodoListApi.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("Connection");
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(connectionString));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
